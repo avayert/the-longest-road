74ed86f17c649dc8831a980748379627ee03e710
Imayhaveborkedit <imayhaveborkedit@users.noreply.github.com>
2022-06-05 16:56:06 -0400

    More work on HexGrid
    I'll consider this more or less done for now until i find more things i need to add

--- lib/catan_game/engine/hexgrid.ex
+++ lib/catan_game/engine/hexgrid.ex
@@ -5,13 +5,9 @@ defmodule Catan.Engine.Hexes.HexGrid do
   alias Catan.Engine.Hexes
   alias Hexes.{HexTile, HexGrid}
 
-  typedstruct do
-    field :tiles, %{{integer(), integer()} => {HexTile.t(), %{}}}, default: %{}
-  end
-
   @type tile :: HexTile.t()
   @type grid :: t()
-  @type coords :: {integer(), integer()}
+  @type coords :: HexTile.axial_coords()
   @type coordlike :: tile | coords
 
   @type axial_offset :: {-1..1, -1..1}
@@ -19,50 +15,71 @@ defmodule Catan.Engine.Hexes.HexGrid do
   @type offset_directions ::
           :top | :topleft | :topright | :bottom | :bottomleft | :bottomright | :left | :right
 
+  typedstruct do
+    field :tiles, %{coords => map()}, default: %{}
+  end
+
   defguard is_grid(item) when is_struct(item, HexGrid)
   defguard is_tile(item) when is_struct(item, HexTile)
   defguard is_coords(item) when is_tuple(item) and tuple_size(item) == 2
   defguard is_coordlike(item) when is_tile(item) or is_coords(item)
+  defguard is_coordlike(i1, i2) when is_coordlike(i1) and is_coordlike(i2)
+  defguard is_coordlike(i1, i2, i3) when is_coordlike(i1, i2) and is_coordlike(i2, i3)
 
   @spec coords_from(coordlike) :: coords
-  @doc "TODO"
+  @doc "Get the coordinates from a coordlike"
   def coords_from(%HexTile{q: q, r: r}), do: {q, r}
   def coords_from({_q, _r} = item) when is_coords(item), do: item
 
   @spec new() :: t()
-  @doc "TODO"
+  @doc "Create a new HexGrid"
   def new() do
     %__MODULE__{}
   end
 
-  @spec init_tile(coordlike, grid) :: {grid, tile}
-  def init_tile(tile, grid) when is_coordlike(tile) and is_grid(grid) do
+  @spec get_data(grid, coordlike) :: map
+  @doc "Get data on a tile from a grid"
+  def get_data(grid, coords) when is_grid(grid) and is_coordlike(coords) do
+    coords = coords_from(coords)
+    Map.get(grid.tiles, coords, %{})
+  end
+
+  @spec put_data(grid, coordlike, map()) :: grid
+  @doc "Insert (merge) data into the tilestate in a grid, overwriting if already present"
+  def put_data(grid, tile, state)
+      when is_grid(grid) and is_coordlike(tile) and is_map(state) do
+    #
     coords = coords_from(tile)
 
-    {_, newtiles} =
-      Map.get_and_update(grid.tiles, coords, fn cv ->
-        {cv, {HexTile.new(coords), %{}}}
+    {_, new_tiles} =
+      Map.get_and_update(grid.tiles, coords, fn cur ->
+        {cur, Map.merge(cur || %{}, state)}
       end)
 
-    {%HexGrid{grid | tiles: newtiles}, Map.get(newtiles, coords)}
+    %HexGrid{grid | tiles: new_tiles}
   end
 
-  @spec put_data(coordlike, map(), grid) :: grid
-  @doc "TODO"
-  def put_data(tile, state, grid)
-      when is_coordlike(tile) and is_map(state) and is_grid(grid) do
-    # TODO: clean this mess up and use init_tile
+  # @spec update_data(grid, coordlike, map()) :: grid
+  # @doc "TODO"
+  # def update_data(grid, tile, state)
+  #     when is_grid(grid) and is_coordlike(tile) and is_map(state) do
+  #   #
+  #   :todo
+  #   grid
+  # end
+
+  @spec clear_data(grid, coordlike) :: grid
+  @doc "Clear tilestate data for a tile, resetting it to an empty map"
+  def clear_data(grid, tile) when is_grid(grid) and is_coordlike(tile) do
     coords = coords_from(tile)
-    {t, ts} = Map.get_lazy(grid.tiles, coords, fn -> {HexTile.new(coords), %{}} end)
-    newstate = Map.put(grid.tiles, coords, {t, Map.merge(ts, state)})
-    %HexGrid{grid | tiles: newstate}
-  end
+    new_tiles = Map.replace(grid.tiles, coords, %{})
 
-  # insert_new
+    %HexGrid{grid | tiles: new_tiles}
+  end
 
   @spec add(coordlike, coordlike) :: tile
   @doc "TODO"
-  def add(a, b) when is_coordlike(a) and is_coordlike(b) do
+  def add(a, b) when is_coordlike(a, b) do
     {q1, r1} = coords_from(a)
     {q2, r2} = coords_from(b)
     HexTile.new({q1 + q2, r1 + r2})
@@ -76,7 +93,7 @@ defmodule Catan.Engine.Hexes.HexGrid do
 
   @spec sub(coordlike, coordlike) :: tile
   @doc "TODO"
-  def sub(a, b) when is_coordlike(a) and is_coordlike(b) do
+  def sub(a, b) when is_coordlike(a, b) do
     {q1, r1} = coords_from(a)
     {q2, r2} = coords_from(b)
     HexTile.new({q1 - q2, r1 - r2})
@@ -125,6 +142,7 @@ defmodule Catan.Engine.Hexes.HexGrid do
   @spec get_diagonal_neighbor(tile, offset_directions) :: tile
   def get_diagonal_neighbor(tile, direction)
       when is_coordlike(tile) and direction in @grid_offset_directions do
+    #
     case direction do
       :top -> {1, -2}
       :topright -> {2, -1}
@@ -147,11 +165,11 @@ defmodule Catan.Engine.Hexes.HexGrid do
   @spec rotate(tile, tile, atom(), pos_integer()) :: tile
   @doc "TODO"
   def rotate(tile, around, direction, turns)
-      when is_coordlike(tile)
-      when is_coordlike(around)
+      when is_coordlike(tile, around)
       when is_atom(direction)
       when direction in @rotate_directions
       when turns > 0 do
+    #
     tile = HexTile.new(tile)
     around = HexTile.new(around)
 
--- test/catan_game/engine/hexgrid_test.exs
+++ test/catan_game/engine/hexgrid_test.exs
@@ -2,24 +2,48 @@ defmodule HexGridTest do
   use ExUnit.Case, async: true
   # doctest Catan.Engine.Hexes.HexGrid
 
-  alias Catan.Engine.Hexes.HexGrid
+  alias Catan.Engine.Hexes.{HexGrid, HexTile}
 
   test "create a grid" do
     assert HexGrid.new() == %HexGrid{}
   end
 
-  test "test guards" do
-  end
-
   test "test coord extraction" do
-    # assert Catan.Engine.Hexes
+    assert HexTile.new(2, 3) |> HexGrid.coords_from() == {2, 3}
+    assert {-1, -2} |> HexGrid.coords_from() == {-1, -2}
   end
 
-  test "init a tile in a grid" do
+  test "put and get data in a grid tile" do
+    coords = {1, 2}
+    data1 = %{type: :a, data: :b}
+    grid = HexGrid.new() |> HexGrid.put_data({1, 2}, data1)
+
+    assert data1 == HexGrid.get_data(grid, coords)
+
+    data2 = %{type: :c}
+    grid = HexGrid.put_data(HexGrid.new(), {1, 2}, data2)
+
+    assert data2 == HexGrid.get_data(grid, coords)
   end
 
-  test "put data in a grid tile" do
+  test "clear tile data in a grid" do
+    coords = {-2, 2}
+    data = %{:type => :bonk, "stuff" => "whatever"}
+    grid1 = HexGrid.new()
+    grid3 = HexGrid.put_data(grid1, coords, data) |> HexGrid.clear_data(coords)
+
+    assert HexGrid.get_data(grid3, coords) == HexGrid.get_data(grid1, coords)
   end
+
+  # test "update tile data in a grid" do
+  #   coords = {6, 0}
+  #   data = %{:type => :bonk, "stuff" => "incorrect"}
+  #   grid1 = HexGrid.put_data(HexGrid.new, coords, data)
+  #   data2 = %{"stuff" => "correct"}
+  #   grid2 = HexGrid.update_data(grid1, coords, data2)
+
+  #   assert Map.merge(data1, data2) == HexGrid.get_data(grid2, coords)
+  # end
 
   @tag :hexmath
   test "add tiles" do
