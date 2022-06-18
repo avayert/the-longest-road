5412b95dfa83ed258da74d390676a7b50a9ff1ab
Imayhaveborkedit <imayhaveborkedit@users.noreply.github.com>
2022-06-18 01:46:18 -0400

    Bugger it

--- lib/catan_game_web/live/main_live.ex
+++ lib/catan_game_web/live/main_live.ex
@@ -1,7 +1,13 @@
 defmodule CatanWeb.MainLive do
   use CatanWeb, :live_view
 
+  @impl true
   def mount(_params, _session, socket) do
     {:ok, socket}
+  end
+
+  @impl true
+  def handle_event("create_lobby", _params, %{assigns: %{}} = socket) do
+    {:noreply, socket}
   end
 end
