26a3cb567fa928b84f432247cb27b9971cfe8734
Imayhaveborkedit <imayhaveborkedit@users.noreply.github.com>
2022-06-25 04:44:35 -0400

    Dumb formatter wont stop eating my macros inside other expressions

--- .formatter.exs
+++ .formatter.exs
@@ -2,5 +2,5 @@
   import_deps: [:ecto, :phoenix, :typed_struct],
   inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
   subdirectories: ["priv/*/migrations"],
-  locals_without_parens: [state: 1, state: 2, action: 1, action: 2]
+  locals_without_parens: [phase: 1, action: 1]
 ]
