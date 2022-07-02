a70beed3d757aa11a930c49a2ed3ae60017d80ec
Imayhaveborkedit <imayhaveborkedit@users.noreply.github.com>
2022-07-02 02:17:35 -0400

    Nevermind the formatter is totally fucked https://github.com/phoenixframework/phoenix_live_view/issues?q=is%3Aissue+whitespace+is%3Aclosed

--- .formatter.exs
+++ .formatter.exs
@@ -1,7 +1,6 @@
 [
-  plugins: [Phoenix.LiveView.HTMLFormatter],
   import_deps: [:ecto, :phoenix, :typed_struct],
-  inputs: ["*.{heex,ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{heex,ex,exs}"],
+  inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
   subdirectories: ["priv/*/migrations"],
   locals_without_parens: [phase: 1, action: 1]
 ]
